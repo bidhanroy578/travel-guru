65200cc2bd8d14bc199c7a97405ad06fa61c39d1
Bidhan Roy <bidhanroy578@gmail.com>
2025-02-24 18:04:25 +0600

    nav bar visibility fixed

--- src/Error.jsx
+++ src/Error.jsx
@@ -4,9 +4,9 @@ import Nav from "./components/nav/Nav";
 const Error = () => {
     const navigate = useNavigate()
     return (
-        <div className="md:p-5 md:px-8">
+        <div className="md:p-5 md:px-8 min-h-screen">
             <Nav></Nav>
-            <h3 className="text-center">Page not found. <Link className="mx-3 text-[#e5a600]" onClick={() =>navigate(-1)}>Go to previous page...</Link> </h3>
+            <h3 className="text-center font-semibold text-3xl p-[30vh]">Page not found. <Link className="mx-3 text-[#e5a600]" onClick={() =>navigate(-1)}>Go to previous page...</Link> </h3>
         </div>
     );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,9 +4,9 @@ import bg from "../../assets/images/Coxbazar.png"
 const Home = () => {
     return (
         <div style={{backgroundImage: `url(${bg})`}} className="bg-cover bg-center min-h-screen">
-            <div className="bg-[#000000b2] min-h-screen md:p-5 md:px-8 space-y-3">
+            <div className="bg-[#000000b2] min-h-screen md:p-5 md:px-8 space-y-3 text-white">
                 <Nav></Nav>
-                <div className=" content-center w-full text-white">
+                <div className=" content-center w-full">
                     <Outlet></Outlet>
                 </div>
             </div>
